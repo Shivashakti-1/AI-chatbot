48ec753664e9c21d62136a36c04e6460b689f433
Shiva Shakti Dubey <149871084+Shivashakti-1@users.noreply.github.com>
2025-04-12 18:38:10 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,60 @@
+import pyttsx3 as p
+import speech_recognition as sr
+import google.generativeai as palm
+
+print("Started")
+engine =p.init()
+rate=engine.getProperty('rate')
+engine.setProperty('rate',180)
+#print(rate)
+
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+#print(voices)
+
+#engine.say("hello sir.  i am ai replica")
+#engine.say("And created by team Ai replica")
+#engine.runAndWait()
+
+def speak(text):
+   engine.say(text)
+   engine.runAndWait()
+
+def ai_respose(prompt): 
+
+    palm.configure(api_key="AIzaSyDiqEPDpI47Qd4Je3I3chb5-z2ZQyKu3gk")
+    response = palm.chat(messages=[str(prompt)])
+    return response.last
+
+r=sr.Recognizer()
+
+speak("I am ai replica , and created by team Ai replica")
+speak("how are you sir?")
+
+with sr.Microphone() as source:
+   r.energy_threshold=10000
+   r.adjust_for_ambient_noise(source,1.2)
+   print("listening")
+   audio= r.listen(source)
+   text=r.recognize_google(audio)
+   print(text)
+
+if "what" and "about" and "you" in text:
+   speak("I am also having good day sir")
+speak("What can i do for you??")
+
+while True:
+      with sr.Microphone() as source:
+        r.energy_threshold=10000
+        r.adjust_for_ambient_noise(source,1.2)
+        print("listening")
+        audio= r.listen(source)
+        text=r.recognize_google(audio)
+        print(text)
+        if text =="bye" or text=="quit" or text =="exit":
+            break
+        response = ai_respose(text)
+        print("Response")
+        print(response)
+        speak(response)
+        speak("What's next sir  ")
